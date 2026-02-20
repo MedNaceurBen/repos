fc0e0d0a72d7be0d534db05f20ec76f269ca5c24
MedNaceurBen <mohamed.naceur.benhassinee@gmail.com>
2026-02-20 10:21:41 +0100

    clean repos

--- dbt-postgres-airflow/dbt/snowflake_data_project/models/staging/stg_orders.sql
+++
@@ -1,8 +0,0 @@
-select 
-    id as order_id,
-    customer_id, 
-    order_date,
-    total_amount,
-    status as order_status
-from 
-    {{ source ('raw_data', 'orders') }}
--- dbt-postgres-airflow/dbt/snowflake_data_project/models/staging/stg_products.sql
+++
@@ -1,7 +0,0 @@
-select
-    id as product_id,
-    name as product_name,
-    category as product_category,
-    price as product_price
-from
-    {{ source ('raw_data', 'products')}}
